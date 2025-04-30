8f7ef25359c3f2fedb23320f89af8be0da737588
Pim Snel <post@pimsnel.com>
2025-04-30 19:28:25 +0200

    wip

--- flake.nix
+++ flake.nix
@@ -1,13 +1,41 @@
 {
   description = "Elastix, getting Nix to the Cloud";
-
   inputs = {
+
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs-oldterraform.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixos-generators.url = "github:nix-community/nixos-generators";
+    nixos-generators.inputs.nixpkgs.follows = "nixpkgs";
     agenix.url = "github:ryantm/agenix";
   };
 
-  outputs = { self, agenix }: {
+  outputs = { self, nixpkgs, nixos-generators, agenix , nixpkgs-oldterraform } :
 
-    nixosModules.default = import ./modules self;
+  let
+    supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; config.allowUnfree = true; overlays = [  ];  });
+
+  in {
+
+    nixosModules = forAllSystems (system:
+    let
+      pkgs = nixpkgsFor.${system};
+    in
+    {
+      default = { config, pkgs, ... }: {
+        imports = [
+
+
+            ];
+        options = {};
+        config = {};
+      };
+
+      #xx = import ./modules self;
+
+    });
+
     lib = import ./lib;
   };
 }
