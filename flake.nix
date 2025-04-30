3557a11719f6fcf3c07997a54a1f0a900ef3a92a
Pim Snel <post@pimsnel.com>
2025-04-30 20:51:58 +0200

    incl nixgenami, wip1

--- flake.nix
+++ flake.nix
@@ -3,13 +3,15 @@
   inputs = {
 
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    nixpkgs-oldterraform.url = "github:NixOS/nixpkgs/nixos-23.05";
+    #nixpkgs-oldterraform.url = "github:NixOS/nixpkgs/nixos-23.05";
     nixos-generators.url = "github:nix-community/nixos-generators";
     nixos-generators.inputs.nixpkgs.follows = "nixpkgs";
     agenix.url = "github:ryantm/agenix";
   };
 
-  outputs = { self, nixpkgs, nixos-generators, agenix , nixpkgs-oldterraform } :
+  outputs = { self, nixpkgs, nixos-generators, agenix
+    # , nixpkgs-oldterraform
+    } :
 
     #    let
     #      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
