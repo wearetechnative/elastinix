c5823e1ffe7b12be9a7d54e5c8e130f806a5cd2b
Pim Snel <post@pimsnel.com>
2025-05-09 17:53:19 +0200

    incl nixgenami, wip1

--- lib/default.nix
+++ lib/default.nix
@@ -1,26 +1,5 @@
+{...}:
 {
-  tf_command2 = import ./tf_command.nix;
-
-  tf_command = {pkgs, prelude, cmd ? "", varsfile ? "" } :
-    let
-      varfile_arg = if varsfile == "" then "" else "-var-file=${varsfile}";
-    in
-    pkgs.writeShellScriptBin "terraform" ''
-      ${prelude}
-      ${pkgs.terraform}/bin/terraform ${cmd} ${varfile_arg} $@
-    '';
-
-  create_bootstrap_img_minimal = { system, minimalModules, nixpkgs, nixos-generators } : nixos-generators.nixosGenerate {
-    inherit system;
-    pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
-    format = "amazon";
-    modules = minimalModules ++ [
-      #"${nixpkgs}/nixos/modules/virtualisation/amazon-image.nix"
-      { amazonImage.name = "nixos_image"; amazonImage.sizeMB = 16 * 1024;}
-    ];
-  };
-
-
-
-
+  tf_command = import ./tf_command.nix;
 }
+
