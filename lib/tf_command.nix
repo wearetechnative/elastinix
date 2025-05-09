b75421ea34bb8db61275a96307143844208b2ff7
Pim Snel <post@pimsnel.com>
2025-05-09 18:56:23 +0200

    incl nixgenami, wip1

--- lib/tf_command.nix
+++ lib/tf_command.nix
@@ -1,9 +1,48 @@
-{ nixpkgs, elastinixModule, ... } : { runSystem, tfBin, cmd ? "", varsfile ? "", bootstrap_img_minimal, ec2conf } :
+{ nixpkgs, elastinixModule, ... } : { runSystem, targetSystem, tfBin, cmd ? "", varsfile ? "", bootstrap_img_minimal, machineFile, bootstrap-config-module } :
 
 let
   varfile_arg = if varsfile == "" then "" else "-var-file=${varsfile}";
 
   pkgs = import nixpkgs { system = runSystem; config.allowUnfree = true; };
+
+  ec2conf = createEC2Host machineFile varsfile;
+
+  #bootstrap-config-module = import ../../lib/ec2nix/nix-modules/10-base-conf-2405.nix;
+
+  minimal-modules = [
+    bootstrap-config-module
+    "${nixpkgs}/nixos/modules/virtualisation/amazon-image.nix"
+  ];
+
+  #  bootstrap_img_minimal = nixos-generators.nixosGenerate {
+  #    system = "x86_64-linux";
+  #    pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+  #    format = "amazon";
+  #    modules = minimal-modules ++ [
+  #      { amazonImage.name = "nixos_image"; amazonImage.sizeMB = 16 * 1024;}
+  #    ];
+  #  };
+
+  createEC2Host = machineFile: tfvarsfile:
+    let
+      defaults = { pkgs, ... }: {
+      };
+
+      liveConfig = (nixpkgs.lib.nixosSystem {
+        inherit targetSystem;
+        specialArgs = { inherit tfvarsfile; ec2orAmi = "ec2"; };
+        modules =
+          minimal-modules ++
+          [
+            defaults
+            (import machineFile)
+            elastinixModule
+          ];
+
+      });
+
+    in liveConfig.config.system.build.toplevel;
+
 
 
   prelude = ''
