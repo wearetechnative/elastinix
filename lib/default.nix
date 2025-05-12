d60ac74634ed2e4759179bbbf7f148785a10760e
Pim Snel <post@pimsnel.com>
2025-05-12 17:50:23 +0200

    incl nixgenami, wip1

--- lib/default.nix
+++ lib/default.nix
@@ -1,9 +1,10 @@
-{nixpkgs, elastinixModule, nixos-generators, ...}:
+{nixpkgs, elastinixModule, nixos-generators, nixpkgs-terraform-1-5-3, ...}:
 {
   tf_command = import ./tf_command.nix {
     inherit nixpkgs;
     inherit elastinixModule;
     inherit nixos-generators;
+    inherit nixpkgs-terraform-1-5-3;
   };
 }
 
