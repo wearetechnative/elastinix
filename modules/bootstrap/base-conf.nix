47d5db88fcc817add0508f922b27ad92e5a328bc
Pim Snel <post@pimsnel.com>
2025-05-12 17:19:32 +0200

    incl nixgenami, wip1

---
+++ modules/bootstrap/base-conf.nix
@@ -0,0 +1,19 @@
+{ rootAuthKeys ? [] } : { pkgs, config, ... } :
+
+{
+  system.stateVersion = "24.05";
+
+  boot.kernel.sysctl = {
+    "vm.max_map_count" = "262144";
+  };
+
+  boot.extraModulePackages = [
+    config.boot.kernelPackages.ena
+  ];
+
+  users.users.root.openssh.authorizedKeys.keys = rootAuthKeys;
+
+  services.openssh.enable = true;
+  services.amazon-ssm-agent.enable = true;
+
+}
