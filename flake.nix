103b54f76e7b33ca1e359cbe6b748e3f229a218b
Caspersonn <lucakasper8@gmail.com>
2025-05-27 15:26:20 +0200

    flake-parts to flake config

--- flake.nix
+++ flake.nix
@@ -9,39 +9,56 @@
     nixos-generators.url = "github:nix-community/nixos-generators/7c60ba4bc8d6aa2ba3e5b0f6ceb9fc07bc261565";
     nixos-generators.inputs.nixpkgs.follows = "nixpkgs";
 
+    nixos-healthchecks.inputs.nixpkgs.follows = "nixpkgs";
+    nixos-healthchecks.url = "github:mrvandalo/nixos-healthchecks";
+
     agenix.url = "github:ryantm/agenix";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
-
-  outputs = {
+  outputs = inputs@{
     self,
+    flake-parts,
     nixpkgs,
-    nixos-generators,
     agenix,
-    nixpkgs-terraform-1-5-3
+    nixos-generators,
+    nixpkgs-terraform-1-5-3,
+    nixos-healthchecks,
+    ...
     }:
-    let
-      elastinixModule = { config, pkgs, ... }:
-        let
-          system = pkgs.stdenv.hostPlatform.system;
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      flake = let
+        elastinixModule = { config, pkgs, ... }:
+          let
+            system = pkgs.stdenv.hostPlatform.system;
+          in {
+            imports = [
+              agenix.nixosModules.default
+              nixos-healthchecks.nixosModules.default
+            ] ++ map (n: "${./modules/programs}/${n}") (builtins.attrNames (builtins.readDir ./modules/programs));
 
-        in {
+            environment.systemPackages = [
+              agenix.packages.${system}.agenix
+              self.packages.${system}.healthchecks
+            ];
 
-          imports = [
-            {
-              environment.systemPackages = [ agenix.packages."${system}".agenix ];
-            }
-            agenix.nixosModules.default
-          ] ++
-            map (n: "${./modules/programs}/${n}") (builtins.attrNames (builtins.readDir ./modules/programs));
-
-          options = {};
-          config = {};
-        };
-
-    in
-      {
-      nixosModules.default = elastinixModule;
-
-      lib = import ./lib { inherit nixpkgs; inherit elastinixModule; inherit nixos-generators; inherit nixpkgs-terraform-1-5-3; };
+            options = {};
+            config = {};
+          };
+      in {
+        nixosModules.default = elastinixModule;
+        lib = import ./lib { inherit nixpkgs elastinixModule nixos-generators nixpkgs-terraform-1-5-3; };
+      };
+      systems = [
+        "x86_64-linux"
+      ];
+      perSystem = { config, pkgs, ... }: {
+        # Recommended: move all package definitions here.
+        # e.g. (assuming you have a nixpkgs input)
+        # packages.foo = pkgs.callPackage ./foo/package.nix { };
+        # packages.bar = pkgs.callPackage ./bar/package.nix {
+        #   foo = config.packages.foo;
+        # };
+      };
     };
 }
