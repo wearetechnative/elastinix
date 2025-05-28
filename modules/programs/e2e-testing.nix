a119b1b303a24a430e4315b9be9dddd744ba9521
Caspersonn <lucakasper8@gmail.com>
2025-05-28 07:39:44 +0200

    Change to e2e testing twenty

--- modules/programs/e2e-testing.nix
+++ modules/programs/e2e-testing.nix
@@ -32,10 +32,11 @@ in {
             pkgs.chromium
         ];
 
+        nixpkgs.config.allowUnfree = true;
+
         environment.variables = {
             LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.libuuid ];
             PLAYWRIGHT_BROWSERS_PATH = "${pkgs.playwright-driver.browsers}";
-            NIXPKGS_ALLOW_UNFREE = 1;
         };
 
 
