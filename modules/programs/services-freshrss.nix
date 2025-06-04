2634329d872e0b66260bfe66f4aec34b77be8b98
Caspersonn <lucakasper8@gmail.com>
2025-06-04 11:11:44 +0200

    freshrss latest config

--- modules/programs/services-freshrss.nix
+++ modules/programs/services-freshrss.nix
@@ -5,36 +5,64 @@ let
 in {
   options.elastinix.programs.freshrss = {
     enable = lib.mkEnableOption "FreshRSS RSS reader";
-    passfile = lib.mkOption {
+    database_passfile = lib.mkOption {
       type = lib.types.str;
-      description = "Location of passfile";
+      description = "Password for database user";
     };
     database_host = lib.mkOption {
       type = lib.types.str;
-      description = "Database hostname";
+      description = "Hostname of the database";
     };
     baseurl = lib.mkOption {
       type = lib.types.str;
       description = "Domain name";
     };
+    passwordfile = lib.mkOption {
+      type = lib.types.str;
+      description = "Password for login user freshrss";
+    };
+    acme_email = lib.mkOption {
+      type = lib.types.str;
+      description = "Email for acme";
+    };
   };
 
   config = lib.mkIf cfg.enable {
+    security.acme = {
+      acceptTerms = true;
+      defaults.email = cfg.acme_email;
+    };
+
+    services.nginx = {
+      enable = true;
+      recommendedGzipSettings = true;
+      recommendedOptimisation = true;
+      recommendedProxySettings = true;
+      recommendedTlsSettings = true;
+      clientMaxBodySize = "25m";
+    };
+
+    services.nginx.virtualHosts."${cfg.baseurl}" = {
+      enableACME = true;
+      forceSSL = true;
+    };
+
     services.freshrss = {
       enable = true;
       package = pkgs.freshrss;
       user = "freshrss";
-      baseUrl = cfg.baseurl;
-      virtualHost = "freshrss";
+      baseUrl = "https://${cfg.baseurl}";
+      virtualHost = cfg.baseurl;
+      passwordFile = cfg.passwordfile;
+      dataDir = "/var/lib/freshrss";
 
       database = {
         name = "freshrss";
         host = cfg.database_host;
         user = "freshrss";
-        passFile = cfg.passfile;
+        passFile = cfg.database_passfile;
         type = "pgsql";
-        port = 3306;
-        tableprefix = "freshrss";
+        port = 5432;
       };
     };
   };
