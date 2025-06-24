f9ef031e47fcdc06245ad301eddd75b7f1056e7a
Pim Snel <post@pimsnel.com>
2025-06-24 19:47:37 +0200

    flkprts wip

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     nixos-generators.url = "github:nix-community/nixos-generators/7c60ba4bc8d6aa2ba3e5b0f6ceb9fc07bc261565";
     nixos-generators.inputs.nixpkgs.follows = "nixpkgs";
 
-    inputs.import-tree.url = "github:vic/import-tree";
+    import-tree.url = "github:vic/import-tree";
 
     #nixos-healthchecks.url = "github:mrvandalo/nixos-healthchecks";
     #nixos-healthchecks.inputs.nixpkgs.follows = "nixpkgs-unstable";
@@ -31,49 +31,58 @@
     #nixos-healthchecks,
     ...
     }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
+    let
+      elastinixModule = { config, pkgs, ... }:
+        let
+          system = pkgs.stdenv.hostPlatform.system;
+        in {
+          imports = [
+            agenix.nixosModules.default
+            #nixos-healthchecks.nixosModules.default
+            {
+              environment.systemPackages = [
+                agenix.packages.${system}.agenix
+                #nixos-healthchecks.packages.${system}.healthchecks
+              ];
+            }
+          ] ++ map (n: "${./modules/programs}/${n}") (builtins.attrNames (builtins.readDir ./modules/programs));
+          options = {};
+          config = {};
+        };
+    in
 
-      imports = [  (import-tree ./flake-modules)  ];
+      flake-parts.lib.mkFlake { inherit inputs; } {
 
-      systems = [
-        "x86_64-linux"
-      ];
+        imports = [
+          inputs.devshell.flakeModule
+          #(import-tree ./flake-modules)
+        ];
 
-      perSystem = { config, self', inputs', pkgs, system, agenix, nixos-generators, nixpkgs-terraform-1-5-3, ... }: {
-      };
+        systems = [
+          "x86_64-linux"
+        ];
 
-      flake = let
-        elastinixModule = { config, pkgs, ... }:
-          let
-            system = pkgs.stdenv.hostPlatform.system;
-          in {
-            imports = [
-              agenix.nixosModules.default
-              #nixos-healthchecks.nixosModules.default
-              {
-                environment.systemPackages = [
-                  agenix.packages.${system}.agenix
-                  #nixos-healthchecks.packages.${system}.healthchecks
-                ];
-              }
-            ] ++ map (n: "${./modules/programs}/${n}") (builtins.attrNames (builtins.readDir ./modules/programs));
-            options = {};
-            config = {};
+        perSystem = _: {
+          devshells.default = {};
+        };
+
+        flake = {
+          nixosModules.default = elastinixModule;
+          lib = import ./lib { inherit nixpkgs elastinixModule nixos-generators nixpkgs-terraform-1-5-3; };
+          nixosModules.stdPrograms = { pkgs, ... }: {
+            imports = [ ./flake-modules/nixos/std-programs.nix ];
           };
-      in {
-        nixosModules.default = elastinixModule;
-        lib = import ./lib { inherit nixpkgs elastinixModule nixos-generators nixpkgs-terraform-1-5-3; };
 
-        #nixosConfigurations.twenty = inputs.nixpkgs.lib.nixosSystem {
-        #  system = "x86_64-linux";
-        #  modules = [
-        #    ./modules/programs/e2e-testing.nix
-        #    nixos-healthchecks.nixosModules.default
-        #    {
-        #      environment.systemPackages = [];
-        #    }
-        #  ];
-        #};
+          #nixosConfigurations.twenty = inputs.nixpkgs.lib.nixosSystem {
+          #  system = "x86_64-linux";
+          #  modules = [
+          #    ./modules/programs/e2e-testing.nix
+          #    nixos-healthchecks.nixosModules.default
+          #    {
+          #      environment.systemPackages = [];
+          #    }
+          #  ];
+          #};
+        };
       };
-    };
 }
