4cafed98017b33040ee742eba57855a96ce88eda
Pim Snel <post@pimsnel.com>
2025-06-27 18:08:41 +0200

    refact createhost

---
+++ lib/os_live_config.nix
@@ -0,0 +1,34 @@
+{inputs, nixpkgs}:
+targetSystem: machineConfig: bootimgModules: tfvarsfile:
+let
+
+  liveConfig = (nixpkgs.lib.nixosSystem {
+    system = targetSystem;
+    specialArgs = { inherit tfvarsfile; ec2orAmi = "ec2"; };
+    modules =
+      bootimgModules ++
+      [
+        {
+          imports = [
+
+            inputs.agenix.nixosModules.default
+            inputs.nixos-healthchecks.nixosModules.default
+
+            (inputs.import-tree ../modules/nixos/programs)
+            (inputs.import-tree ../modules/nixos/services)
+            (inputs.import-tree ../modules/nixos/tests)
+          ];
+
+          environment.systemPackages = [
+            inputs.agenix.packages.${targetSystem}.agenix
+          ];
+        }
+
+        machineConfig
+
+      ];
+
+  });
+
+in
+  liveConfig.config.system.build.toplevel
