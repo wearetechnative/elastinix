5dfeb4b8c9bc9f68a09717547060b92a48712971
Pim Snel <post@pimsnel.com>
2025-06-30 18:20:24 +0200

    fix options

--- modules/nixos/services/service-locate.nix
+++ modules/nixos/services/service-locate.nix
@@ -1,9 +1,9 @@
 { pkgs, config, lib, ... }:
 
 {
-  options.elastinix.locate.enable = lib.mkEnableOption "enable locate service";
+  options.elastinix.services.locate.enable = lib.mkEnableOption "enable locate service";
 
-  config = lib.mkIf config.elastinix.locate.enable {
+  config = lib.mkIf config.elastinix.services.locate.enable {
     services = {
       locate.enable = true;
       locate.package = pkgs.mlocate;
