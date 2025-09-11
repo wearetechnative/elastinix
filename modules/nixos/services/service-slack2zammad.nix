07dd4179f2fac0e80a6d970ba80131b85a43814e
Caspersonn <lucakasper8@gmail.com>
2025-09-11 14:25:23 +0200

    temp move away slack2zammad

--- modules/nixos/services/service-slack2zammad.nix
+++ modules/nixos/services/service-slack2zammad.nix
@@ -5,21 +5,21 @@ let
 in
 
   {
-  options.elastinix.services.slack2zammad = {
-
-    enable = lib.mkEnableOption "slack to zammad";
-
-    environment_file = lib.mkOption {
-      type = lib.types.str;
-      description = "";
-    };
-  };
-
-  config = lib.mkIf cfg.enable {
-
-    services.slack2zammad = {
-      enable  = true;
-      envFile = cfg.environment_file;
-    };
-  };
+    #  options.elastinix.services.slack2zammad = {
+    #
+    #    enable = lib.mkEnableOption "slack to zammad";
+    #
+    #    environment_file = lib.mkOption {
+    #      type = lib.types.str;
+    #      description = "";
+    #    };
+    #  };
+    #
+    #  config = lib.mkIf cfg.enable {
+    #
+    #    services.slack2zammad = {
+    #      enable  = true;
+    #      envFile = cfg.environment_file;
+    #    };
+    #  };
 }
