274b49b832645e9584f42e5013379ca049d5158c
jerpen80 <dra_penny@yahoo.com>
2025-10-09 13:38:09 +0200

    replaced tf command with tf command ami

---
+++ lib/tf_command_ami.nix
@@ -0,0 +1,28 @@
+{ inputs, ... } :
+  { nixpkgs,
+    runSystem,
+    machineConfig ? {},
+    targetSystem ? "x86_64-linux",
+    tfBinOverride ? "",
+    terraformBinConf ? { distribution = "terraform"; version = "1-5-3"; },
+    cmd ? "apply",
+    varsfile ? "" ,
+    rootAuthorizedKeys ? [] } :
+let
+
+  pkgsRunSys = import nixpkgs { system = runSystem; };
+
+  useTfBin = (import ./tf_bin.nix {inherit inputs; }) (terraformBinConf // { inherit nixpkgs runSystem tfBinOverride; });
+
+  bootstrapImage = (import ./os_config_bootstrap.nix { inherit inputs nixpkgs; }) targetSystem rootAuthorizedKeys;
+  liveConfig = (import ./os_config_live.nix { inherit inputs; }) { inherit nixpkgs targetSystem rootAuthorizedKeys machineConfig varsfile;};
+
+  tf_prelude = ''
+    export TF_VAR_ec2_bootstrap_img_path="${bootstrapImage}/nixos_image.vhd";
+    export TF_VAR_ec2_host_live_path="${liveConfig.config.system.build.toplevel}"
+  '';
+
+  tf_varfile_arg = if (cmd == "apply" || cmd == "plan" ) then "-var-file=${varsfile}" else "";
+in
+
+pkgsRunSys.writeShellScriptBin "terraform" '' ${tf_prelude} ${useTfBin} ${cmd} ${tf_varfile_arg} $@''
