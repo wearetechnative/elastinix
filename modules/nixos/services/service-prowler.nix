122db43bbc848af50cb00ef82d19d626e70537cc
jerpen80 <dra_penny@yahoo.com>
2025-10-14 10:26:40 +0200

    make prowler service

--- modules/nixos/services/service-prowler.nix
+++ modules/nixos/services/service-prowler.nix
@@ -1,9 +1,19 @@
 { pkgs, config, lib, ... }:
+
 {
-  environment.systemPackages = [
+  options.elastinix.services.prowlerDashboard.enable = lib.mkEnableOption "enable prowler dashboard service";
+
+  config = lib.mkIf config.elastinix.services.prowlerDashboard.enable {
+    environment.systemPackages = [
         pkgs.prowler
         pkgs.awscli2
     ];
-  networking.firewall.allowedTCPPorts = [11666];
+    networking.firewall.allowedTCPPorts = [11666];
+    systemd.services.prowlerDashboard = {
+        serviceConfig.Type = "oneshot";
+        wantedBy = [ "multi-user.target" ];
+        script = '' HOST=0.0.0.0 prowler dashoard ''
+    };
+  };
 
 }
