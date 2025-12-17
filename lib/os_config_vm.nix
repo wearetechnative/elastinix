bd9d82c030e72c7647d3146b10dd1cf20a59f0f4
Pim Snel <post@pimsnel.com>
2025-12-17 12:28:14 +0100

    backport runvm

---
+++ lib/os_config_vm.nix
@@ -0,0 +1,60 @@
+{inputs}:
+  { nixpkgs, targetSystem, machineConfig, varsfile, rootAuthorizedKeys ? [],... }:
+
+inputs.nixos-generators.nixosGenerate {
+  system = targetSystem;
+
+  pkgs = import nixpkgs { system = targetSystem; config.allowUnfree = true; };
+  format = "qcow";
+  modules =
+    let
+      tfvars = if varsfile == ""
+        then
+        {}
+      else
+        builtins.fromJSON (builtins.readFile varsfile);
+    in
+
+      [
+        {
+          _module.args.nixpkgs = nixpkgs;
+          _module.args.tfvars = tfvars;
+          _module.args.targetSystem = targetSystem;
+        }
+        {
+          virtualisation.diskSize = 8 * 1024;
+
+          fileSystems."/" = {
+            device = "/dev/disk/by-label/nixos";
+            fsType = "ext4";
+            autoResize = true;
+          };
+
+          boot.growPartition = true;
+          boot.kernelParams = [ "console=ttyS0" ];
+          boot.loader.grub.device = "/dev/vda";
+          boot.loader.timeout = 0;
+
+          users.extraUsers.root.password = "";
+        }
+
+        "${nixpkgs}/nixos/modules/profiles/qemu-guest.nix"
+        #"${nixpkgs}/nixos/modules/virtualisation/amazon-image.nix"
+        (import ../modules/nixos/bootstrap/base-conf.nix rootAuthorizedKeys)
+
+        inputs.agenix.nixosModules.default
+        inputs.nixos-healthchecks.nixosModules.default
+
+        (inputs.import-tree ../modules/nixos/programs)
+        (inputs.import-tree ../modules/nixos/services)
+        (inputs.import-tree ../modules/nixos/tests)
+
+        {
+          environment.systemPackages = [
+             inputs.agenix.packages.${targetSystem}.agenix
+          ];
+        }
+
+        machineConfig
+  ];
+}
