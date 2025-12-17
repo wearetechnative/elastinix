ae2e8deda8117e954a2711cc8afe845973a40393
TorreiroW <githubwto@toorren.net>
2025-12-17 08:40:05 +0100

    add jsonify-aws-dotfiles

--- lib/os_config_live.nix
+++ lib/os_config_live.nix
@@ -27,7 +27,7 @@ let
 
         inputs.agenix.nixosModules.default
         inputs.slack2zammad.nixosModules.slack2zammad
-        inputs.jsonify-aws-dotfiles.nixosModules.jsonify-aws-dotfiles
+        inputs.jsonify-aws-dotfiles.nixosModules.default
         inputs.nixos-healthchecks.nixosModules.default
 
         (inputs.import-tree ../modules/nixos/programs)
@@ -37,8 +37,8 @@ let
         {
           environment.systemPackages = [
              inputs.agenix.packages.${targetSystem}.agenix
-             inputs.slack2zammad.packages.${targetSystem}.slack2zammad
              inputs.jsonify-aws-dotfiles.packages.${targetSystem}.jsonify-aws-dotfiles
+             inputs.slack2zammad.packages.${targetSystem}.slack2zammad
           ];
         }
 
