9c65cfbaffc5383d5a4ca50bc4f65fa6543cba1e
Pankhuri22-P <pankhurip30@gmail.com>
2026-03-24 12:09:12 +0100

    changed the timing of psql-backup because it should not run at the same time as log rotate

--- modules/nixos/services/service-psqldump.nix
+++ modules/nixos/services/service-psqldump.nix
@@ -40,7 +40,7 @@ in {
     systemd.timers."${infra_environment}-psql-backup" = {
       wantedBy = [ "timers.target" ];
       timerConfig = {
-        OnCalendar = "*-*-* 23:00:00";
+        OnCalendar = "*-*-* 23:30:00";
         Unit = "${infra_environment}-psql-backup.service";
       };
     };
