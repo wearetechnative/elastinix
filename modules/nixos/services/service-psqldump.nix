0862eaf4717dc6b1e8c507246273ed3c0ca1a9d4
Pankhuri22-P <pankhurip30@gmail.com>
2026-04-01 10:19:40 +0200

    changed the psql backup service timing

--- modules/nixos/services/service-psqldump.nix
+++ modules/nixos/services/service-psqldump.nix
@@ -40,7 +40,7 @@ in {
     systemd.timers."${infra_environment}-psql-backup" = {
       wantedBy = [ "timers.target" ];
       timerConfig = {
-        OnCalendar = "*-*-* 23:30:00";
+        OnCalendar = "*-*-* 00:30:00";
         Unit = "${infra_environment}-psql-backup.service";
       };
     };
