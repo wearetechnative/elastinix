2436bb9caa4143ba87dab18f66a56bae10ba1452
Caspersonn <lucakasper8@gmail.com>
2026-04-16 15:43:19 +0200

    Builtin to string for chhoto port

--- modules/nixos/services/service-chhoto.nix
+++ modules/nixos/services/service-chhoto.nix
@@ -41,7 +41,7 @@ in
       forceSSL = true;
       locations = {
         "/" = {
-          proxyPass = "http://127.0.0.1:${cfg.port}";
+          proxyPass = "http://127.0.0.1:${builtins.toString cfg.port}";
         };
       };
     };
