276e56f2794170bc29d97c22137f58c7e86f3d0e
Caspersonn <lucakasper8@gmail.com>
2026-05-04 15:57:44 +0200

    Merge conflict fix

---
+++ modules/nixos/services/service-chhoto.nix
@@ -0,0 +1,49 @@
+{ lib, config, tfvars, ... }:
+let
+  cfg = config.elastinix.services.chhoto;
+  environment_domain = tfvars.environment_domain;
+in
+{
+  options.elastinix.services.chhoto = {
+
+    enable = lib.mkEnableOption "Chhoto URL shortener";
+
+    environmentFiles = lib.mkOption {
+      type = lib.types.listOf lib.types.path;
+      default = [];
+      description = "Environment files for secrets (password, API key).";
+    };
+
+    port = lib.mkOption {
+      type = lib.types.port;
+      description = "Port for the chhoto-url server.";
+    };
+
+    siteUrl = lib.mkOption {
+      type = lib.types.nullOr lib.types.str;
+      default = "chhoto.${environment_domain}";
+      description = "External URL where chhoto-url is publicly accessible.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.chhoto-url = {
+      enable = true;
+      environmentFiles = cfg.environmentFiles;
+      settings = {
+        port = cfg.port;
+        site_url = cfg.site_url;
+      };
+    };
+
+    services.nginx.virtualHosts."chhoto.${environment_domain}" = {
+      enableACME = true;
+      forceSSL = true;
+      locations = {
+        "/" = {
+          proxyPass = "http://127.0.0.1:${builtins.toString cfg.port}";
+        };
+      };
+    };
+  };
+}
