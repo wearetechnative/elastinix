4b777791dd44abc0f8d6c541383a18d38cf50e53
Pankhuri22-P <pankhurip30@gmail.com>
2026-06-16 14:16:05 +0200

    added cache directory to vulnix scan service

--- modules/nixos/services/service-vulnix-scan.nix
+++ modules/nixos/services/service-vulnix-scan.nix
@@ -20,7 +20,7 @@ in {
         echo "Generating package manifest from /run/current-system..."
 
         echo "Scanning packages for known CVEs..."
-        ${pkgs.vulnix}/bin/vulnix --from-file /var/lib/sbom/packages.json > /var/lib/sbom/output.json || EXIT=$?
+        ${pkgs.vulnix}/bin/vulnix --cache-dir /var/lib/sbom/cache --from-file /var/lib/sbom/packages.json > /var/lib/sbom/output.json || EXIT=$?
 
         if [ "''${EXIT:-0}" -eq 2 ]; then
           echo "Vulnerabilities found. See /var/lib/sbom/system.json for details."
