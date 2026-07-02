9d86a16f23068161038f59a7309811a4bf9090e4
Caspersonn <lucakasper8@gmail.com>
2026-07-02 15:30:19 +0200

    feat: Add multiworkspace support on twenty

--- modules/nixos/services/docker-twenty.nix
+++ modules/nixos/services/docker-twenty.nix
@@ -20,6 +20,14 @@ in {
       description = "The port that should be used";
     };
 
+    default_subdomain = lib.mkOption {
+      type = lib.types.str;
+      default = "app";
+      description = ''
+        Twenty's DEFAULT_SUBDOMAIN in multi-workspace mode.
+      '';
+    };
+
     # Server
     server_environment_file = lib.mkOption {
       type = lib.types.str;
@@ -45,6 +53,7 @@ in {
     virtualisation.oci-containers.containers."twenty" = {
       image = "twentycrm/twenty:${cfg.version}";
       ports = [ "${cfg.forward_port}:3000" ];
+      environment.DEFAULT_SUBDOMAIN = cfg.default_subdomain;
       environmentFiles = [ cfg.server_environment_file ];
       dependsOn = [ ];
       volumes = [
@@ -64,6 +73,7 @@ in {
 
     virtualisation.oci-containers.containers."twenty-worker" = {
       image = "twentycrm/twenty:${cfg.version}";
+      environment.DEFAULT_SUBDOMAIN = cfg.default_subdomain;
       environmentFiles = [ cfg.worker_environment_file ];
       dependsOn = [ "twenty" ];
       volumes = [
@@ -77,8 +87,41 @@ in {
       ];
     };
 
+    security.acme.certs."twenty.${environment_domain}" = {
+      domain = "*.twenty.${environment_domain}";
+      extraDomainNames = [ "twenty.${environment_domain}" ];
+      dnsProvider = "route53";
+      dnsResolver = "1.1.1.1:53";
+      group = "nginx";
+    };
+
+    systemd.services."acme-order-renew-twenty.${environment_domain}".environment = {
+      AWS_REGION = "eu-central-1";
+    };
+
     services.nginx.virtualHosts."twenty.${environment_domain}" = {
-      enableACME = true;
+      useACMEHost = "twenty.${environment_domain}";
+      forceSSL = true;
+      extraConfig = ''
+        client_max_body_size 50M;
+      '';
+      locations = {
+        "/" = {
+          proxyPass = "http://127.0.0.1:${cfg.forward_port}";
+          proxyWebsockets = true;
+          extraConfig = ''
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto $scheme;
+            proxy_buffering off;
+            proxy_read_timeout 86400;
+          '';
+        };
+      };
+    };
+
+    services.nginx.virtualHosts."*.twenty.${environment_domain}" = {
+      useACMEHost = "twenty.${environment_domain}";
       forceSSL = true;
       extraConfig = ''
         client_max_body_size 50M;
