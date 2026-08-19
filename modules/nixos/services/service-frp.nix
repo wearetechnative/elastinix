9814378abcf27a05e11ce4c85100a891be8f0487
Caspersonn <lucakasper8@gmail.com>
2026-08-19 08:10:45 +0200

    fix: invokeai uses internal ip

--- modules/nixos/services/service-frp.nix
+++ modules/nixos/services/service-frp.nix
@@ -32,7 +32,7 @@ in
       forceSSL = true;
       locations = {
         "/" = {
-          proxyPass = "http://invokeai.ainative.eu:7070";
+          proxyPass = "http://127.0.0.1:7070"; # FRP vhostHTTPPort on this host; Host header ($host=invokeai.ainative.eu) drives FRP subdomain routing
           extraConfig = ''
             auth_basic "Restricted Content";
             proxy_pass_request_headers on;
@@ -51,7 +51,7 @@ in
       forceSSL = true;
       locations = {
         "/" = {
-          proxyPass = "http://openwebui.ainative.eu:7070";
+          proxyPass = "http://127.0.0.1:7070"; # FRP vhostHTTPPort on this host; Host header ($host=openwebui.ainative.eu) drives FRP subdomain routing
           extraConfig = ''
             proxy_set_header Upgrade           $http_upgrade;
             proxy_set_header Connection        $connection_upgrade;
@@ -66,7 +66,7 @@ in
       forceSSL = true;
       locations = {
         "/" = {
-          proxyPass = "http://test.ainative.eu:7070";
+          proxyPass = "http://127.0.0.1:7070"; # FRP vhostHTTPPort on this host; Host header ($host=test.ainative.eu) drives FRP subdomain routing
         };
       };
     };
