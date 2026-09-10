1d5a5de1fdf5cbf09ee757701cb5c4558cb34863
Caspersonn <lucakasper8@gmail.com>
2026-09-10 11:32:29 +0200

    Nebula service with lighthouse compatibility

---
+++ modules/nixos/services/service-nebula.nix
@@ -0,0 +1,39 @@
+{ config, lib, pkgs, ... }:
+
+{
+  options.elastinix.services.nebula-lighthouse = {
+    enable = lib.mkEnableOption "Nebula Lighthouse";
+    keyPath = lib.mkOption {
+      type = lib.types.path;
+      description = "path to key";
+    };
+
+    caCertPath = lib.mkOption {
+      type = lib.types.path;
+      description = "path to ca certificate";
+    };
+    certPath = lib.mkOption {
+      type = lib.types.path;
+      description = "path to certificate";
+    };
+    isLighthouse = lib.mkOption {
+      type = lib.types.bool;
+      description = "Enable/disable lighthouse";
+    };
+  };
+
+  config = lib.mkIf config.elastinix.services.nebula.enable {
+    environment.systemPackages = with pkgs; [ nebula ];
+    services.nebula.networks.mesh = {
+      enable = true;
+      isLighthouse = config.elastinix.services.nebula.isLighthouse;
+      cert = config.elastinix.services.nebula.certPath;
+      key = config.elastinix.services.nebula.keyPath;
+      ca = config.elastinix.services.nebula.caCertPath;
+    };
+
+    networking.firewall = {
+      allowedUDPPorts = [ 4242 ];
+    };
+  };
+}
